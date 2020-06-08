fe1791d04bd4522df86591c9306a6aba09a01ec8
Mickalu <45756345+Mickalu@users.noreply.github.com>
2020-06-08 16:51:34 +0200

    on a plus de valeur string

--- replace_attack_value.py
+++ replace_attack_value.py
@@ -8,6 +8,11 @@ Created on Sun Jun  7 18:46:12 2020
 
 import pandas as pd
 
+def change_string_int_value(df, colonne):
+    for indx, val in enumerate(df[colonne].unique()):
+        df[colonne].replace(to_replace = val, value = indx, inplace = True)
+
+
 df = pd.read_csv("KDD_with_name.csv")
 
 DDoS =  ['neptune', 'teardrop', 'smurf', 'pod', 'back', 'land']
@@ -17,6 +22,11 @@ attack = ['warezclient', 'ipsweep', 'portsweep', 'nmap', 'satan', 'guess_passwd'
 df['attack_name'].replace(to_replace = DDoS, value = 'DDoS', inplace = True)
 df['attack_name'].replace(to_replace = attack, value = 'attack', inplace = True)
 
-df.to_csv('KDD_attack_name_change.csv', index=False) 
+change_string_int_value(df, 'protocol_type')
+change_string_int_value(df, 'service')
+change_string_int_value(df, 'flag')
+
+
+df.to_csv('KDD_attack_name_change.csv', index = False)
 
 
