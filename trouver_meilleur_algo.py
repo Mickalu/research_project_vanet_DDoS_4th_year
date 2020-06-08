2a7a9ed9018cc86786989c93456b69573b048ecc
Mickalu <45756345+Mickalu@users.noreply.github.com>
2020-06-08 17:29:10 +0200

    start find better algo

--- trouver_meilleur_algo.py
+++ trouver_meilleur_algo.py
@@ -6,7 +6,10 @@ Created on Sun Jun  7 19:42:48 2020
 """
 
 import pandas as pd
+
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -17,16 +20,23 @@ warnings.filterwarnings("ignore") #pour ignorer les warning dans la console
 
 df = pd.read_csv("KDD_attack_name_change.csv")
 
+
 X = df.drop('attack_name', axis = 1)
 Y = df['attack_name']
 
+mlp = MLPClassifier(hidden_layer_sizes = (64,32,16), random_state = 1)
 dt = DecisionTreeClassifier(min_samples_leaf = 0.15, random_state = 1)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3, random_state = 20)
+classifiers = [('DecisionTreeClassifier',dt), ('MLPClassifier', mlp)]
 
-dt.fit(X_train, Y_train)
-dt_predict = dt.predict(X_test)
-dt_accuracy_score = accuracy_score(Y_test, dt_predict)
+for clf_name, clf in classifiers:
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3, random_state = 20)
+    
+    clf.fit(X_train, Y_train)
+    clf_predict = clf.predict(X_test)
+    clf_accuracy_score = accuracy_score(Y_test, clf_predict)
+    
+    print(clf_name, " accuracy score : ",clf_accuracy_score)
 
-print(dt_accuracy_score)
+
 
