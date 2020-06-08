831045f33bde9684fd35f1377df2c863597c06f2
Mickalu <45756345+Mickalu@users.noreply.github.com>
2020-06-08 18:17:25 +0200

    ajout de RMSE

--- trouver_meilleur_algo.py
+++ trouver_meilleur_algo.py
@@ -9,10 +9,13 @@ import pandas as pd
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error as MSE
 
+from sklearn.metrics import classification_report, confusion_matrix
 ##############################################################
 import warnings
 warnings.filterwarnings("ignore") #pour ignorer les warning dans la console
@@ -26,17 +29,27 @@ Y = df['attack_name']
 
 mlp = MLPClassifier(hidden_layer_sizes = (64,32,16), random_state = 1)
 dt = DecisionTreeClassifier(min_samples_leaf = 0.15, random_state = 1)
+rfc = RandomForestClassifier(n_estimators = 50, random_state = 1)
 
-classifiers = [('DecisionTreeClassifier',dt), ('MLPClassifier', mlp)]
+classifiers = [('DecisionTreeClassifier',dt), ('MLPClassifier', mlp), ('RandomForestClassifier', rfc)]
+
 
 for clf_name, clf in classifiers:
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3, random_state = 20)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3, random_state = 1)
     
     clf.fit(X_train, Y_train)
     clf_predict = clf.predict(X_test)
+    
     clf_accuracy_score = accuracy_score(Y_test, clf_predict)
     
+    print(confusion_matrix(Y_test, clf_predict))
+    print(classification_report(Y_test, clf_predict))
+    
     print(clf_name, " accuracy score : ",clf_accuracy_score)
+    
+    mse_clf = MSE(clf_predict, Y_test)
+    rmse_clf = mse_clf ** (1/2)
+    print('RMSE test using', clf_name ,': ', rmse_clf)
 
 
 
